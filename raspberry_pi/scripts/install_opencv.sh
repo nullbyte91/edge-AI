0749c916321fb57405c61b3d92ebb3352c2bf8d1
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2020-06-07 22:45:34 +0530

    RPI opencv install auto script

---
+++ raspberry_pi/scripts/install_opencv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Tested on RPI3 + raspbian
+pip3 install opencv-python
+sudo apt-get install python-dev libatlas-base-dev
+sudo apt install python3-opencv
+sudo apt install libjasper1
+sudo apt install libqtgui4
+sudo apt install libqt4-test
+sudo apt-get install libatlas-base-dev 
+sudo apt-get install libjasper-dev 
+pip3 install opencv-contrib-python==4.1.0.25
